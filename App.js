8ad96e2d641dfe6638ff260a13f2e607b51c471b
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2018-01-22 12:20:29 +0900

    init

---
+++ App.js
@@ -0,0 +1,108 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+	Platform,
+	StyleSheet,
+	Text,
+	Image,
+	View
+} from 'react-native';
+
+const instructions = Platform.select({
+	ios: 'Press Cmd+R to reload,\n' +
+	'Cmd+D or shake for dev menu',
+	android: 'Double tap R on your keyboard to reload,\n' +
+	'Shake or press menu button for dev menu',
+});
+
+class Greeting extends Component {
+	render() {
+		return (
+			<Text style={styles[this.props.styleKey || 'bigblue']}>Hello {this.props.name}!</Text>
+		);
+	}
+}
+
+class Blink extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {isShowingText: true}
+
+		setInterval(() => {
+			this.setState(previousState => {
+				return { isShowingText: !previousState.isShowingText };
+			})
+		}, 1000)
+	}
+
+	render() {
+		let display = this.state.isShowingText ? this.props.text : ' ';
+		return (
+			<Text>{display}</Text>
+		)
+	}
+}
+
+export default class App extends Component<{}> {
+	render() {
+		let pic = {
+			uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+		};
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>
+					Welcome to React Native!!!
+				</Text>
+				<Text style={styles.instructions}>
+					To get started, edit App.js
+				</Text>
+				<Text style={styles.instructions}>
+					{instructions}
+				</Text>
+				<View>
+					<Blink text='I love to blink' />
+					<Blink text='Yes blinking is so great' />
+					<Blink text='Why did they ever take this out of HTML' />
+					<Blink text='Look at me look at me look at me' />
+				</View>
+				<Image source={pic} style={{width: 193, height: 110}}/>
+				<Greeting name='Rexxar' styleKey='bigblue' />
+				<Greeting name='Jaina' styleKey='red' />
+				<Greeting name='Valeera' />
+			</View>
+		);
+	}
+}
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
+	bigblue: {
+		color: 'blue',
+		fontWeight: 'bold',
+		fontSize: 30,
+	},
+	red: {
+		color: 'red',
+	},
+});
